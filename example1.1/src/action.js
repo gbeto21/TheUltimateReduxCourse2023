092b0036a5ace2091581362b5b0920940519923e
Alberto Gonzalez <albertoglezb@hotmail.com>
2023-01-23 09:29:15 -0600

    The create action was modified to use the updated version.

--- example1.1/src/action.js
+++ example1.1/src/action.js
@@ -1,13 +1,21 @@
 import * as actionTypes from "./actionTypes"
+import { createAction } from "@reduxjs/toolkit"
 
-export const addTask = (task) => {
-  return { type: actionTypes.ADD_TASK, payload: { task } }
-}
+const test = createAction(actionTypes.ADD_TASK)
+// console.log(test({ task: "Task 2" }))
 
-export const removeTask = (id) => {
-  return { type: actionTypes.REMOVE_TASK, payload: { id } }
-}
+export const addTask = createAction(actionTypes.ADD_TASK)
+export const removeTask = createAction(actionTypes.REMOVE_TASK)
+export const completedTask = createAction(actionTypes.TASK_COMPLETED)
 
-export const completedTask = (id) => {
-  return { type: actionTypes.TASK_COMPLETED, payload: { id } }
-}
+// export const addTask = (task) => {
+//   return { type: actionTypes.ADD_TASK, payload: { task } }
+// }
+
+// export const removeTask = (id) => {
+//   return { type: actionTypes.REMOVE_TASK, payload: { id } }
+// }
+
+// export const completedTask = (id) => {
+//   return { type: actionTypes.TASK_COMPLETED, payload: { id } }
+// }
--- example1.1/src/index.js
+++ example1.1/src/index.js
@@ -1,11 +1,11 @@
 import store from "./store"
 import { addTask, removeTask, completedTask } from "./action"
 
-store.dispatch(addTask("Task 1"))
+store.dispatch(addTask({ task: "Task 1" }))
 console.log(store.getState())
 
-store.dispatch(completedTask(1))
+store.dispatch(completedTask({ id: 1 }))
 console.log(store.getState())
 
-store.dispatch(removeTask(1))
+store.dispatch(removeTask({ id: 1 }))
 console.log(store.getState())
