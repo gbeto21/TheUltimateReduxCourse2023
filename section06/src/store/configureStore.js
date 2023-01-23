d4f8acdb01b4e697e8348dae07e64409406ba162
Alberto Gonzalez <albertoglezb@hotmail.com>
2023-01-23 10:51:37 -0600

    The first middleware was created.

--- section06/src/store/configureStore.js
+++ section06/src/store/configureStore.js
@@ -1,12 +1,14 @@
-import { configureStore } from "@reduxjs/toolkit";
-import employeeReducer from "./employees";
-import taskReducer from "./tasks";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit"
+import employeeReducer from "./employees"
+import taskReducer from "./tasks"
+import log from "../middleware/log"
 
 const store = configureStore({
-    reducer: {
-        tasks: taskReducer,
-        employees: employeeReducer,
-    },
-});
+  reducer: {
+    tasks: taskReducer,
+    employees: employeeReducer,
+  },
+  middleware: (getDefaultMiddleware) => [...getDefaultMiddleware(), log],
+})
 
-export default store;
+export default store
